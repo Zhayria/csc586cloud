600a555bc08cc0d85840232c5dc7d8b33de53a95
Zhayria <89924086+Zhayria@users.noreply.github.com>
2023-06-15 18:54:51 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,25 +1,28 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
-
+         
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 
-prefixForIp = "192.168.1."
+prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 # Create a XenVM
 for i in range(2):
-   if i == 0:
-      node = request.XenVM("control")
-   else:
-       node = request.XenVM("host")
+  if i == 0:
+    node = request.XenVM("control")
+  else: 
+    node = request.XenVM("host")
+    
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
-  iface.addAddress(rspec.IPv4Address(prefixForIP +str(i+1), "255.255.255.0"))
-` link.addInterface(iface)
-
-
+  iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  #if i == 0:
+    #node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
+    
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
