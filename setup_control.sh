ffa6adfb4c512b0e45fee71b1a0bdc5071e0d957
Zhayria <89924086+Zhayria@users.noreply.github.com>
2023-06-29 19:02:01 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -2,7 +2,7 @@
 
 #generate passwordless SSH
 runuser -u zw920902 -- ssh-keygen -q -t rsa -f /users/zw920902/.ssh/id_rsa -N ''
-runuser -u zw920902 -- cat /users/zw920902/.ssh/id_rsa.pub >> /users/zw92902/.ssh/authorized_keys
+runuser -u zw920902 -- cat /users/zw920902/.ssh/id_rsa.pub >> /users/zw920902/.ssh/authorized_keys
 
 # setup NFS for key sharing
 apt update
