abe404f2b48b5ada9432b29402925e8b95df3c12
Zhayria <89924086+Zhayria@users.noreply.github.com>
2023-06-29 17:24:55 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 LOG_FILE="/var/webserver_monitor/unauthorized.log"
-ADMIN_EMAIL="admin@example.com"
+ADMIN_EMAIL="zhayriaw80@gmail.com"
 
 # Check if there are new entries in the log file
 if [ -s "$LOG_FILE" ]; then
     SUBJECT="New Unauthorized Access Detected"
     MESSAGE=$(cat "$LOG_FILE")
 else
-    SUBJECT="No Unauthorized Access"
-    MESSAGE="No unauthorized access detected."
+    SUBJECT="Unauthorized Access"
+    MESSAGE="Unauthorized access detected."
 fi
 
-# Send email to admin
+# Send email 
 echo "$MESSAGE" | mail -s "$SUBJECT" "$ADMIN_EMAIL"
